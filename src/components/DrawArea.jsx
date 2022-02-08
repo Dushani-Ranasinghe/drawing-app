ec9524e65849ed97ca8b45b2a66a1abf80ef8109
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-08 22:18:14 +0530

    Set rendering context to 2D

--- src/components/DrawArea.jsx
+++ src/components/DrawArea.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React,{ useLayoutEffect , useState } from 'react';
 import '../Styles/DrawArea.css'
 
 function DrawArea() {
-  return <div>
-      <canvas id="canvas">
+    useLayoutEffect(()=>{
+        //retrive node in DOM which reprecent <canvas> element using element id
+        const canvas = document.getElementById("canvas");
+        //access to drawing context for 2D graphics
+        const ctx = canvas.getContext("2d");
 
+        
+    });
+  return <div>
+      
+      <canvas id="canvas" width={window.innerWidth} height={window.innerHeight}>
+      
       </canvas>
   </div>;
 }
