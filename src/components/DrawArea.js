c619e7d14fc2f39b6338b99796bac8f277bda788
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-24 16:59:38 +0530

    modified

--- src/components/DrawArea.js
+++ src/components/DrawArea.js
@@ -1,20 +1,31 @@
 import React,{ useLayoutEffect , useState } from 'react';
 import '../Styles/DrawArea.css'
-import { Stage, Layer } from "react-konva";
+import { Stage, Layer, Line, Text } from "react-konva";
 
 
 function DrawArea() {
    
-   
+    const [lines, setLines] = React.useState([]);
   return <div>
       <Stage
      width={window.innerWidth}
-     height={window.innerHeight}
-     onMouseDown={handleMouseDown}
-     onMousemove={handleMouseMove}
-     onMouseup={handleMouseUp}>
-        
-     
+     height={window.innerHeight}>
+         <Layer>
+          <Text text="Just start drawing" x={5} y={30} />
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="#df4b26"
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+              globalCompositeOperation={
+                line.tool === "eraser" ? "destination-out" : "source-over"
+              }
+            />
+          ))}
+        </Layer>
       </Stage>
       
   </div>;
