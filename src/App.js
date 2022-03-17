ba7cb702c8b43d91bc2e80e8a55f2ad076deb4ef
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-17 11:14:53 +0530

    Created a simple canvas and toolbar

--- src/App.js
+++ src/App.js
@@ -1,64 +1,189 @@
-import './App.css';
-import "./Styles/Canvas.css"
-import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Drawing app </h1>
+import React, { Component } from "react";
+import { Stage, Layer, Arrow, Circle, Line } from "react-konva";
+import "./Canvas.css";
 
-    {/* Create a Konva canvas */}
-    <div className='canvas-container'>
-      {/* set canvas width and height */}
-      <Stage width={window.innerWidth} height={window.innerHeight} className="canvas-stage">
 
-       {/* create a layer on stage */}
-       <Layer>
-         {/* test text on canvas */}
-         <Text 
-          text='This is the text on the canvas'
-          fontSize={20}
-         />
-
-         {/* test rectangle on canvas */}
-         <Rect
-          x={100}
-          y={40}
-          width={100}
-          height={70}
-          fill="yellow"
-          stroke="black"
-         />
-
-        {/* test circle on canvas */}
-        <Circle
-        x={350}
-        y={70}
-        radius={50}
-        fill="red"
-        />
-
-        {/* create a shape using line in konva */}
-        <Line
-        x={500}
-        y={40}
-        points={[0, 0, 100, 0, 100, 100]}
-        tension={0.08}
-        closed
-        stroke="pink"
-        fillLinearGradientStartPoint={{ x: -50, y: -50 }}
-          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
-          fillLinearGradientColorStops={[0, 'white', 1, 'purple']}
-        />
-       </Layer>
-      </Stage>
-
-         
-  
-
-    </div>
-    </div>
-  );
+class Drawable {
+  constructor(startx, starty) {
+    this.startx = startx;
+    this.starty = starty;
+  }
 }
 
-export default App;
+// Arrow drawing function
+class ArrowDrawable extends Drawable {
+  constructor(startx, starty) {
+    super(startx, starty);
+    this.x = startx;
+    this.y = starty;
+  }
+  registerMovement(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  render() {
+    const points = [this.startx, this.starty, this.x, this.y];
+    return <Arrow points={points} fill="black" stroke="black" />;
+  }
+}
+
+class CircleDrawable extends ArrowDrawable {
+  constructor(startx, starty) {
+    super(startx, starty);
+    this.x = startx;
+    this.y = starty;
+  }
+  render() {
+    const dx = this.startx - this.x;
+    const dy = this.starty - this.y;
+    const radius = Math.sqrt(dx * dx + dy * dy);
+    return (
+      <Circle radius={radius} x={this.startx} y={this.starty} stroke="black" />
+    );
+  }
+}
+class FreePathDrawable extends Drawable {
+  constructor(startx, starty) {
+    super(startx, starty);
+    this.points = [startx, starty];
+  }
+  registerMovement(x, y) {
+    this.points = [...this.points, x, y];
+  }
+  render() {
+    return <Line points={this.points} fill="black" stroke="black" />;
+  }
+}
+class Eraser extends Drawable{
+  constructor(startx, starty) {
+    super(startx, starty);
+    this.points = [startx, starty];
+  }
+  registerMovement(x, y) {
+    this.points = [...this.points, x, y];
+  }
+  render() {
+    return <Line points={this.points} fill="white" stroke="white" />;
+  }
+}
+class SceneWithDrawables extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawables: [],
+      newDrawable: [],
+      newDrawableType: "FreePathDrawable"
+    };
+  }
+  getNewDrawableBasedOnType = (x, y, type) => {
+    const drawableClasses = {
+      FreePathDrawable,
+      ArrowDrawable,
+      CircleDrawable,
+      Eraser
+    };
+    return new drawableClasses[type](x, y);
+  };
+  handleMouseDown = e => {
+    const { newDrawable } = this.state;
+    if (newDrawable.length === 0) {
+      const { x, y } = e.target.getStage().getPointerPosition();
+      const newDrawable = this.getNewDrawableBasedOnType(
+        x,
+        y,
+        this.state.newDrawableType
+      );
+      this.setState({
+        newDrawable: [newDrawable]
+      });
+    }
+  };
+  handleMouseUp = e => {
+    const { newDrawable, drawables } = this.state;
+    if (newDrawable.length === 1) {
+      const { x, y } = e.target.getStage().getPointerPosition();
+      const drawableToAdd = newDrawable[0];
+      drawableToAdd.registerMovement(x, y);
+      drawables.push(drawableToAdd);
+      this.setState({
+        newDrawable: [],
+        drawables
+      });
+    }
+  };
+  handleMouseMove = e => {
+    const { newDrawable } = this.state;
+    if (newDrawable.length === 1) {
+      const { x, y } = e.target.getStage().getPointerPosition();
+      const updatedNewDrawable = newDrawable[0];
+      updatedNewDrawable.registerMovement(x, y);
+      this.setState({
+        newDrawable: [updatedNewDrawable]
+      });
+    }
+  };
+  render() {
+
+    const drawables = [...this.state.drawables, ...this.state.newDrawable];
+    return (
+      <div>
+
+      {/* Tool bar */}
+        <button
+          onClick={e => {
+            this.setState({ newDrawableType: "ArrowDrawable" });
+          }}
+        >
+          Arrow
+        </button>
+        <button
+          onClick={e => {
+            this.setState({ newDrawableType: "CircleDrawable" });
+          }}
+        >
+          Circle
+        </button>
+        <button
+          onClick={e => {
+            this.setState({ newDrawableType: "FreePathDrawable" });
+          }}
+        >
+          Pencil
+        </button>
+        <button
+          onClick={e => {
+            this.setState({ newDrawableType: "Eraser" });
+          }}
+        >
+          Eraser
+        </button>
+       
+
+       {/* Drawing area */}
+ <Stage
+          onMouseDown={this.handleMouseDown}
+          onMouseUp={this.handleMouseUp}
+          onMouseMove={this.handleMouseMove}
+          width={900}
+          height={700}
+        >
+          <Layer>
+            {drawables.map(drawable => {
+              return drawable.render();
+            })}
+          </Layer>
+        </Stage>
+      </div>
+    );
+  }
+}
+
+// App function
+
+function App() {
+  return <SceneWithDrawables />;
+}
+
+export default App
+
